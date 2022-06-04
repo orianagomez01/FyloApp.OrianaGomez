91b7af96ce909f02fb2c9fbdbf095c857f3737fa
orianagomez01 <66321241+orianagomez01@users.noreply.github.com>
2022-06-03 21:18:18 -0300

    error con objetos en form

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,11 +1,17 @@
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import React, { useState } from "react";
 import { useCartContext } from "../context/CartContext";
+import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
+import { Button, Modal } from "react-bootstrap";
 
 const Checkout = () => {
   const [orderId, setOrderId] = useState("");
-  const { cart, getTotal, cartLength } = useCartContext();
+  const { cart, getTotal, cartLength, clear } = useCartContext();
   const [final, setFinal] = useState(false);
+
+  const handleClose = () => setFinal(false);
+  const handleShow = () => setFinal(true);
 
   const [buyer, setBuyer] = useState({
     nombre: "",
@@ -13,6 +19,7 @@ const Checkout = () => {
     edad: "",
     telefono: "",
     email: "",
+    emailConfirm: "",
   });
 
   const dateOrder = new Date().toLocaleDateString();
@@ -28,8 +35,6 @@ const Checkout = () => {
   }
 
   const saveToFirestore = (order) => {
-    // setFinal(true);
-
     const db = getFirestore();
     const orderCollection = collection(db, "orders");
 
@@ -37,7 +42,6 @@ const Checkout = () => {
       setOrderId(response);
       console.log(response);
     });
-    // .finally(() => setFinal(false));
   };
 
   const submitOnChange = (e) => {
@@ -52,78 +56,152 @@ const Checkout = () => {
   };
 
   return (
-    // <>
-    <div className="checkout checkout__container">
-      <div className="checkout__content--one">
-        <h1> Resumen</h1>
-        <div className="checkout__description">
-          <div className="checkout_row">
-            <p> Cantidad de items:</p>
-            <p className="checkout__cart checkout__paragraphs">
-              {" "}
-              {cartLength()}
-            </p>
-          </div>
-          <div className="checkout_row">
-            <p className="checkout__paragraph"> Gastos de envío: </p>
-            <p className="checkout__cart checkout__paragraphs checkout__paragraph">
-              {" "}
-              ¡Envío gratis!
-            </p>
-          </div>
-          <div className="checkout_row">
-            <p className="checkout__paragraphs"> TOTAL:</p>
-            <p className="checkout__cart checkout__paragraphs">
-              {" "}
-              ${getTotal()}
-            </p>
+    <>
+      <div className="checkout checkout__container">
+        <div className="checkout__content--one">
+          <h1> Resumen</h1>
+          <div className="checkout__description">
+            <div className="checkout_row">
+              <p> Cantidad de items:</p>
+              <p className="checkout__cart checkout__paragraphs">
+                {" "}
+                {cartLength()}
+              </p>
+            </div>
+            <div className="checkout_row">
+              <p className="checkout__paragraph"> Gastos de envío: </p>
+              <p className="checkout__cart checkout__paragraphs checkout__paragraph">
+                {" "}
+                ¡Envío gratis!
+              </p>
+            </div>
+            <div className="checkout_row">
+              <p className="checkout__paragraphs"> TOTAL:</p>
+              <p className="checkout__cart checkout__paragraphs">
+                {" "}
+                ${getTotal()}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="checkout__content--two">
-        <h2 className="checkout__title">Detalles de facturación</h2>
-        {Object.keys(buyer).map((key) => {
-          return (
-            <div key={key}>
-              <form>
-                <input
-                  className="checkout__input"
-                  placeholder={key}
-                  name={key}
-                  value={buyer[key]}
-                  type="text"
-                  required
-                  onChange={submitOnChange}
-                />
-              </form>
-            </div>
-          );
-        })}
+        <div className="checkout__content--two">
+          <h2 className="checkout__title">Detalles de facturación</h2>
+          {/* {Object.keys(buyer).map((key) => {
+            return (
+              <div key={key}> */}
+          <form>
+            <input
+              className="checkout__input"
+              id="nombre"
+              type="text"
+              name="nombre"
+              required
+              onChange={submitOnChange}
+              placeholder="Nombre"
+            />
+            <input
+              className="checkout__input"
+              id="apellido"
+              type="text"
+              name="apellido"
+              required
+              onChange={submitOnChange}
+              placeholder="Apellido"
+            />
+            <input
+              className="checkout__input"
+              id="edad"
+              type="text"
+              name="edad"
+              required
+              onChange={submitOnChange}
+              placeholder="Edad"
+            />
+            <input
+              className="checkout__input "
+              id="telefono"
+              type="tel"
+              name="telefono"
+              required
+              onChange={submitOnChange}
+              placeholder="Teléfono (xxxxxxxxx)"
+            />
+            <input
+              className="checkout__input"
+              id="email"
+              type="email"
+              name="email"
+              required
+              onChange={submitOnChange}
+              placeholder="E-mail"
+            />
+            <input
+              className="checkout__input "
+              id="emailConfirm"
+              type="email"
+              name="emailConfirm"
+              required
+              onChange={submitOnChange}
+              placeholder="Confirmar e-mail"
+            />
+
+            {/* <input
+                    className="checkout__input"
+                    placeholder={key}
+                    name={key}
+                    value={buyer[key]}
+                    type="text"
+                    required
+                    onChange={submitOnChange}
+                  /> */}
+          </form>
+        </div>
+        {/* );
+          })} */}
         <button
           className="checkout__btn"
           disabled={validateForm()}
           onClick={() => {
-            orderHandler(), setFinal(true);
+            orderHandler();
+            handleShow();
           }}
         >
           Realizar compra{" "}
         </button>
       </div>
-    </div>
+      {/* </div> */}
+      {/* {Object.keys(buyer).map((key) => {
+        return ( */}
+      <Modal show={final} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>¡Tu orden fue confirmada!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          ¡Muchas gracias por su compra {buyer.nombre}! Se ha enviado a su mail{" "}
+          {buyer.email} el e-ticket con el orden de compra: {orderId}{" "}
+        </Modal.Body>
+        <Modal.Footer>
+          <Link to="/">
+            {" "}
+            <Button variant="success" onClick={handleClose}>
+              {" "}
+              Volver al inicio
+            </Button>
+          </Link>
+        </Modal.Footer>
+      </Modal>
+      );
+      {/* })} */}
+    </>
   );
 };
 
-export default Checkout;
+// html: (
+//   <p>
+//     ¡Muchas gracias por su compra ${buyer.nombre}! Se ha enviado a su
+//     mail ${buyer.email} el e-ticket con el orden de compra: {orderId}
+//   </p>
+// ),
+// confirmButtonText: "Volver al inicio",
 
-{
-  /* <div className="checkout__final">
-        <p>
-          {" "}
-          ¡Muchas gracias por su compra {buyer.nombre}! Se ha enviado a su mail{" "}
-          {buyer.email} el e-ticket con el orden de compra: {orderId}{" "}
-        </p>
-      </div> */
-}
-{
-  /* </> */
-}
+export default Checkout;
